d4ccdcaca44bc72fbc5489d6cb454f5e87e0bf62
Logon PF Fiap <logonpflocal@fiap.com.br>
2019-06-08 12:38:01 -0300

    Inclusao da tela de login

--- app/src/main/java/br/com/heiderlopes/IluminacaoActivity.kt
+++ app/src/main/java/br/com/heiderlopes/IluminacaoActivity.kt
@@ -2,6 +2,7 @@ package br.com.heiderlopes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_iluminacao.*
@@ -19,6 +20,8 @@ class IluminacaoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_iluminacao)
+
+        FirebaseAuth.getInstance().signOut()
 
         database = FirebaseDatabase.getInstance()
         ledRef = database.getReference("led")
--- app/src/main/java/br/com/heiderlopes/LoginActivity.kt
+++ app/src/main/java/br/com/heiderlopes/LoginActivity.kt
@@ -1,12 +1,69 @@
 package br.com.heiderlopes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        if (usuarioLogado()) {
+            proximaTelaLogada()
+        }
+
+        botaoEntrar.setOnClickListener {
+            validarUsuario()
+        }
+    }
+
+    private fun validarUsuario() {
+        val email = inputEmail.editText?.text.toString()
+        val senha = inputSenha.editText?.text.toString()
+        if(email.isEmpty()) {
+            inputEmail.isErrorEnabled = true
+            inputEmail.error = "Favor preencher o e-mail"
+            return
+        } else {
+            inputEmail.isErrorEnabled = false
+        }
+
+        if(senha.isEmpty()) {
+            inputSenha.isErrorEnabled = true
+            inputSenha.error = "Favor preencher a senha"
+            return
+        } else {
+            inputSenha.isErrorEnabled = false
+        }
+
+        mAuth.signInWithEmailAndPassword(
+            email,
+            senha)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    proximaTelaLogada()
+                } else {
+                    Toast.makeText(this, "Falha na autenticação",
+                        Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    private fun usuarioLogado(): Boolean {
+        return mAuth.currentUser != null
+    }
+
+    private fun proximaTelaLogada() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
